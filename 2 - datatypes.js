89bb6d2f8955b351a354584a90cf80817d0513bd
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 09:43:14 +0000

    Create 2 - datatypes.js

---
+++ 2 - datatypes.js
@@ -0,0 +1,17 @@
+/* Data types:
+- Undefined
+- Null
+- Boolean
+- String
+- Symbol
+- Number
+- Object
+*/
+
+// String - Any form of text
+// Number - A number
+// Undefined - Something that hasn't been defined
+// Null - nothing
+// Boolean - true or false value
+// Symbol - Immutable primitive value that is unique
+// Object - Something that can store key-value pairs.
