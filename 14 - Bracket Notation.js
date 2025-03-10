b7ccf3f94f9040f41ed948339f74c3de242c5144
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 11:15:42 +0000

    Update 14 - Bracket Notation.js

--- 14 - Bracket Notation.js
+++ 14 - Bracket Notation.js
@@ -5,7 +5,37 @@ firstLetterOfFirstName = firstName[0];
 console.log(firstLetterOfFirstName)
 
 var firstLetterOfLastName = "";
-var lastName = "Lovalace";
+var lastName = "Lovelace";
 
 firstLetterOfLastName = lastName[0]
 console.log(firstLetterOfLastName)
+
+//---------------------------------------//
+
+// Bracket notation to find the nth character:
+
+var thirdLetterOfLastName = "";
+var lastName = "Lovelace";
+
+thirdLetterOfLastName = lastName[3]
+console.log(thirdLetterOfLastName)
+
+//---------------------------------------//
+
+// Bracket notation to find the last character in a string:
+
+var lastLetterOfLastName = "";
+var lastName = "Lovelace";
+
+lastLetterOfLastName = lastName[lastName.length - 1]
+console.log(lastLetterOfLastName)
+
+//---------------------------------------//
+
+// Bracket notation to find the Nth-to-Last Character
+
+var thirdToLastLetterOfLastName = "";
+var lastName = "Lovelace";
+
+thirdToLastLetterOfLastName = lastName[lastName.length - 3]
+console.log(thirdToLastLetterOfLastName)
