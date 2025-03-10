fdda8fd5eb250af5f91e08e9a14a432b79ec1cce
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 10:13:55 +0000

    Create 6 - Basic Math.js

---
+++ 6 - Basic Math.js
@@ -0,0 +1,11 @@
+var sum = 10 + 10;
+console.log(sum)
+
+var difference = 10-10;
+console.log(difference)
+
+var product = 10*10;
+console.log(product)
+
+var quotient = 10/10;
+console.log(quotient)
