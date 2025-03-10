f3eea1255b4993ba1fad128f4238871384e45636
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 10:05:58 +0000

    Create 5 - Case Sensitivity in Variables.js

---
+++ 5 - Case Sensitivity in Variables.js
@@ -0,0 +1,18 @@
+// Variable names in javascript are case sensitive. 
+
+// Declarations:
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+// Assignments
+STUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A String";
+tITLEcASEoVER = 9000;
+
+// The above assignments will not work with the declarations. 
+// The below will, though:
+
+StUdLyCapVaR = 10;
+properCamelCase = "A String";
+TitleCaseOver = 9000;
