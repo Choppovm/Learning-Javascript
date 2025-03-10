be8e4d9338cce239de0cf7da8856ca85a09045f2
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 09:52:02 +0000

    Update and rename 2 - datatypes.js to 2 - declare variables.js

---
+++ 2 - declare variables.js
@@ -0,0 +1,36 @@
+/* Data types:
+- Undefined
+- Null
+- Boolean
+- String
+- Symbol
+- Number
+- Object
+*/
+
+// String - Any form of text
+// Number - A number
+// Undefined - Something that hasn't been defined
+// Null - nothing
+// Boolean - true or false value
+// Symbol - Immutable primitive value that is unique
+// Object - Something that can store key-value pairs.
+
+//------------------------------------------------------//
+
+// One way to declare a variable is using the var keyword:
+var myName = "Charlie"
+myName = 8
+myName = "Charles"
+
+// Another way is using let:
+let myAge = 17
+
+// And another way is using const
+const pi = 3.14159
+
+/* Here are the differences: 
+  - Var: can be used throughout the whole program
+  - Let: only used within the scope of where it's declared 
+  - Const: a variable that can't and shouldn't ever be changed
+*/
