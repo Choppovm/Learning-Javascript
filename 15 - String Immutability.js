59c738eb3e82049147aab80b72e2bdf59998811f
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 11:10:50 +0000

    Create 15 - String Immutability.js

---
+++ 15 - String Immutability.js
@@ -0,0 +1,10 @@
+// Strings are immutable
+
+var myString = "Jello World";
+myString[0] = "H";
+
+// The above code will NOT work.
+// Instead, the code below will.
+
+var myString = "Jello World":
+myString = "Hello World"
