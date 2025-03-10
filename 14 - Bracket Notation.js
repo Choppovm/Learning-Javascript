2b2e595ecd0d893615c0a580e31d38b35e517466
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 11:09:18 +0000

    Create 14 - Bracket Notation.js

---
+++ 14 - Bracket Notation.js
@@ -0,0 +1,11 @@
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+console.log(firstLetterOfFirstName)
+
+var firstLetterOfLastName = "";
+var lastName = "Lovalace";
+
+firstLetterOfLastName = lastName[0]
+console.log(firstLetterOfLastName)
