850d9abb03b2b6f72ff38d28cf3fc5b98351bc15
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 10:20:17 +0000

    Update 6 - Basic Math.js

--- 6 - Basic Math.js
+++ 6 - Basic Math.js
@@ -1,11 +1,24 @@
 var sum = 10 + 10;
 console.log(sum)
+// can also be written as:
+sum += 10;
+console.log(sum)
+
 
 var difference = 10-10;
+console.log(difference)
+// can also be written as:
+difference -= 10
 console.log(difference)
 
 var product = 10*10;
 console.log(product)
+// can als obe written as:
+product *= 10
+console.log(product)
 
 var quotient = 10/10;
+console.log(quotient)
+// can also be written as:
+quotient /= 10
 console.log(quotient)
