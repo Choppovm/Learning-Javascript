0ff1a79b1b04570de98d8fda2c3086fd74626941
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 09:56:44 +0000

    Create 4 - Uninitialized Variables.js

---
+++ 4 - Uninitialized Variables.js
@@ -0,0 +1,14 @@
+// The below variables are uninitialised
+var a;
+var b;
+var c;
+
+// The below variables are initialised
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not touch below
+a = a + 1;
+b = b + 5;
+c = c + " string!";
