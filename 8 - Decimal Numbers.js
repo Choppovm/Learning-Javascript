7a61237fabdf30944b6c12c6c8712d92a2a08669
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 10:18:08 +0000

    Create 8 - Decimal Numbers.js

---
+++ 8 - Decimal Numbers.js
@@ -0,0 +1,19 @@
+var myDecimal = 8.7;
+
+var mySecondDecimal = 0.0009;
+
+// Multiplying:
+
+var product = 2.0 * 2.5;
+console.log(product)
+
+// Dividing:
+
+var quotient = 4.4 / 2.2;
+console.log(quotient)
+
+// Finding a remainder:
+
+var remainder;
+remainder = 11 % 3;
+console.log(remainder)
