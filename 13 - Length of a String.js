3f5f6ae023288eb7ab406dab0119ae40c3fddb28
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-10 11:06:43 +0000

    Create 13 - Length of a String.js

---
+++ 13 - Length of a String.js
@@ -0,0 +1,14 @@
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+console.log(firstNameLength)
+
+//-----------------------------------------------//
+
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+lastNameLength = lastName.length;
+console.log(lastNameLength)
+
