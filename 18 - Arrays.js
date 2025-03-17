236ae41ec2cb56b84f6a06daea5ab21f6077710a
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-17 11:21:24 +0000

    Create 18 - Arrays.js

---
+++ 18 - Arrays.js
@@ -0,0 +1,15 @@
+// Storing Multiple Values with Arrays
+var ourArray = ["John", 23]
+
+var myArray = ["Charlie", 17];
+
+//---------------------------------------//
+
+// Nested Arrays
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+var myArray = [["Bulls", 23], ["White Sox", 45]];
+
+//---------------------------------------//
+
+// Access Array Data with Indexes
