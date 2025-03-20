8701bd7a692bd5e004ee33a0b1af2c58972b38c0
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-20 09:39:39 +0000

    Create 20 - If statements.js

---
+++ 20 - If statements.js
@@ -0,0 +1,16 @@
+functuon ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+}
+
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
