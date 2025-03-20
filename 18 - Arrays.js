9b6c166f4a136135e08a462b7ca13b806266a01a
Charlie Vernor Miles <118126633+Choppovm@users.noreply.github.com>
2025-03-20 09:35:26 +0000

    Update 18 - Arrays.js

--- 18 - Arrays.js
+++ 18 - Arrays.js
@@ -83,3 +83,18 @@ ourArray.unshift("Happy");
 var myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
 myArray.unshift(["Paul", 35]);
+
+//---------------------------------------//
+
+// Stand in Line
+
+function nextInLine(arr, item) {
+  arr.push(item);
+  return arr.shift();
+}
+
+var testArr = [1,2,3,4,5]
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console,log("After: " + JSON.stringify(testArr));
